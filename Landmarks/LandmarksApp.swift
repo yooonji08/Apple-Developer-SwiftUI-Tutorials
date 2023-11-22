c8f8bda7185cb9af1edfa5f55538a2b4262c5468
yooonji08 <yooonji08@naver.com>
2023-11-22 21:18:27 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 이윤지 on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
