47dfc0859d68085393ee453bf047b0a3e2c82c1b
yooonji08 <yooonji08@naver.com>
2023-11-24 15:54:45 +0900

    Feat: MapView, CircleImage 등을 활용하여 상세 화면 구성하기

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,18 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            // MapView 불러오고, 높이 300
+            // 높이 지정을 안하면, 화면 전체에 MapView를 띄움
+            MapView()
+                .frame(height: 300)
+            
+            // 원형 이미지의 높이가 -130, padding을 하단에 -130
+            // 이미지를 위쪽으로 이동하여 텍스트를 위한 공간을 만듦
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                // VStack전체에 대하여 한번에 폰트, 색상을 설정
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider() // 구분선
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
+            .padding()
+            
+            // 전체 VStack콘텐츠를 화면 상단으로 올려주기 위한 스페이서
+            Spacer()
         }
-        .padding()
     }
 }
 
