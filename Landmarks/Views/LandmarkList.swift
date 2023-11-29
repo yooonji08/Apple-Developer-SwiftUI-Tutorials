9633eb94b6bf2068b775e905ff9c5ca577c1d964
yooonji08 <yooonji08@naver.com>
2023-11-29 23:16:34 +0900

    Feat: 다양한 방법으로 정적 뷰, 동적 뷰 만들기

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 이윤지 on 11/29/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
