cf796495d0f4a69ab1bfcdbeb8e64e7463e19ec9
yooonji08 <yooonji08@naver.com>
2023-11-29 22:45:26 +0900

    Feat: Preview를 보는 두 가지 방법(단일 행, Group)

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -22,6 +22,20 @@ struct LandmarkRow: View {
     }
 }
 
-#Preview {
+// 미리보기에 이름을 지정하면 쉽게 구분 가능
+/*#Preview("Turtle Rock") {
     LandmarkRow(landmark: landmarks[0]) // landmarkData파일에 있는 첫번째 json의 값을 가져옴
+}
+
+#Preview("Salmon") {
+    LandmarkRow(landmark: landmarks[1]) // landmarkData파일에 있는 두번째 json 값을 가져옴
+}*/
+
+// Group - 콘텐츠를 그룹화해주는 컨테이너
+// 캔버스에 하나의 미리보기로 쌓인 그룹의 하위 뷰를 렌더링해줌
+#Preview {
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
 }
