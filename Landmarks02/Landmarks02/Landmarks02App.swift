38987c566199d6205f75f4dc4a3ff2e1aadba8b8
Yoonji Lee <yooonji08@naver.com>
2024-01-03 16:04:39 +0900

    Build: test

---
+++ Landmarks02/Landmarks02/Landmarks02App.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks02App.swift
+//  Landmarks02
+//
+//  Created by 이윤지 on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Landmarks02/Landmarks02/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks02
+//
+//  Created by 이윤지 on 1/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
