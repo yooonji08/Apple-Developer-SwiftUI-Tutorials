10f96a7c795a8f38bb5a7e6dec753ecccac1dc19
Yoonji Lee <yooonji08@naver.com>
2024-01-03 15:09:35 +0900

    Build: Landmark 프로젝트 재생성-3

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -1,18 +1,18 @@
 //
-//  ContentView.swift
+//  LandmarkDetail.swift
 //  Landmarks
 //
-//  Created by 이윤지 on 1/3/24.
+//  Created by 이윤지 on 11/30/23.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct LandmarkDetail: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ContentView()
+    LandmarkDetail()
 }
