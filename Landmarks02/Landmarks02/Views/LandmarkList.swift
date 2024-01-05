33da646be8b289716fe3063334df2e051e59954d
Yoonji Lee <yooonji08@naver.com>
2024-01-05 14:06:08 +0900

    Feat: 별 표시가 되어있는 랜드마크만 목록으로 보기
    - showFavoritesOnly 변수로 별 표시 랜드마크만 보는 것을 true, false 조정 가능

--- Landmarks02/Landmarks02/Views/LandmarkList.swift
+++ Landmarks02/Landmarks02/Views/LandmarkList.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    // 별 표시한 랜드마크만 볼지 말지에 대한 여부를 지정하는 변수
+    // 변수의 값을 true로 바꾸면, 별 표시가 되어 있는 랜드마크만 리스트로 띄워줌
+    @State private var showFavoritesOnly = false
+    
+    // 속성과 값을 확인하여 랜드마크 목록의 필터링된 버전(별 표시가 있는 것)을 계산함
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         // 세로 방향 리스트(정적 뷰)
         /*List {
@@ -20,10 +31,9 @@ struct LandmarkList: View {
             LandmarkRow(landmark: landmark)
         }*/
         
-        //
         NavigationSplitView {
             // 세로 방향 리스트(동적 뷰) - Landmar    Identifiable 프로토콜을 정의한 경우
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink { // 각 행을 링크로 연결
                     LandmarkDetail(landmark: landmark) // 각 행을 클릭하면 세부 화면으로 이동
                 } label: { // 리스트에 들어갈 정보를 담음
