6f6b6829af1767ab3528c7b9b957438207fb11dc
Yoonji Lee <yooonji08@naver.com>
2024-01-18 15:48:02 +0900

    Feat: ContentView 공부하기

--- MyApp/MyApp/MyApp/ContentView.swift
+++ MyApp/MyApp/MyApp/ContentView.swift
@@ -7,15 +7,25 @@
 
 import SwiftUI
 
+// ContentView: 가장 최상단 view
+// View: 화면에 그려질 뷰
 struct ContentView: View {
+    // 다음과 같은 파일에서는(View를 사용하는 파일) body에 View 프로토콜을 사용
     var body: some View {
+        // VStack:
+        // -> 수직 정렬(Vertical Stack), 주로 하위 뷰 수가 적을 때 사용
+        // -> 반대 개념(HStack: 수평 정렬)
+        // Lazy VStack:
+        // -> 하위 뷰가 많을 때 사용(뷰를 렌더링해서 사용)
+        // -> 반대 개념(Lazy HStack)
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "globe") // 이미지, 시스템에서 기본으로 제공하는 이미지 사용(sf symbols에서 원하는 이미지명 찾아서 입력하면 사용 가능)
+                .imageScale(.large) // 뷰(VStack) 내의 이미지 크기를 상대적인 크기로 조정(small, medium, large)
+                .foregroundStyle(.tint) // 이미지의 색상 지정 (foregroundColor는 descreted 예정이므로, 대신 foregroundStyle 사용)
+            Text("Hello, world!") // 텍스트
         }
-        .padding()
+        .padding() // 안쪽 여백 추가
+        //.padding([.bottom, .trailing], 20) // 여백을 주고 싶은 곳에 여백 지정 가능
     }
 }
 
