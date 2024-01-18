36745bec7762b1109c6538b5434d47ebf36f0ba9
Yoonji Lee <yooonji08@naver.com>
2024-01-18 14:12:12 +0900

    Feat: 각 사진의 페이지 추적(currentPage)

--- Landmarks02/Landmarks02/Views/PageView/PageViewController.swift
+++ Landmarks02/Landmarks02/Views/PageView/PageViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 // page유형의 배열 저장 => 랜드마크 간 스크롤을 하는데 사용하는 페이지
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
+    @Binding var currentPage: Int
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
@@ -21,15 +22,17 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
         pageViewController.dataSource = context.coordinator
+        pageViewController.delegate = context.coordinator
         
         return pageViewController
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
-        pageViewController.setViewControllers([context.coordinator.controllers[0]], direction: .forward, animated: true)
+        // 현재 페이지 값을 업데이트
+        pageViewController.setViewControllers([context.coordinator.controllers[currentPage]], direction: .forward, animated: true)
     }
     
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
         var controllers = [UIViewController]()
         
@@ -64,5 +67,18 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             }
             return controllers[index + 1]
         }
+        
+        // 페이지 전환이 될 때마다, 해당 메서드 호출함
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            didFinishAnimating finished: Bool,
+            previousViewControllers: [UIViewController],
+            transitionCompleted completed: Bool) {
+                if completed,
+                   let visibleViewController = pageViewController.viewControllers?.first,
+                   let index = controllers.firstIndex(of: visibleViewController) {
+                    parent.currentPage = index
+                }
+            }
     }
 }
